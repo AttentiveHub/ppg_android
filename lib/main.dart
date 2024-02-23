6de517ebf43f09e82c8d6ce14262eee5c739d7aa
Adnan Zaki <68474810+adnanzaki19@users.noreply.github.com>
2024-02-22 17:43:55 -0700

    Channel choice confirmation

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Polar PPG App',
+      title: 'Polar Logger2 App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Colors.green,
       ),
-      home: const MyHomePage(title: 'Polar PPG Connection Page'),
+      home: const MyHomePage(title: 'Polar Logger2 Page'),
     );
   }
 }
@@ -38,6 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
   String? _selectedDeviceId;
   String? _connectedDeviceId;
   String _connectionStatus = "Disconnected";
+  final List<String> _channels = ['HR', 'ACC', 'ECG', 'GYR', 'PPG', 'PPI', 'MAG'];
+  List<int> _selectedChannelIndices = [];
+  bool _channelsConfirmed = false; // Tracks whether the selection has been confirmed
 
   @override
   void initState() {
@@ -61,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
               toastLength: Toast.LENGTH_SHORT,
               gravity: ToastGravity.CENTER,
               timeInSecForIosWeb: 1,
-              textColor: Colors.white,
+              textColor: status == "Connected" ? Colors.greenAccent : Colors.red,
               fontSize: 16.0,
             );
           }
@@ -85,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setStateDialog) {
             return AlertDialog(
-              title: const Text("Select a Device"),
+              title: const Text("Choose a Sensor"),
               content: SingleChildScrollView(
                 child: StreamBuilder<List<Map<String, String>>>(
                   stream: _devicesStreamController.stream,
@@ -96,9 +100,8 @@ class _MyHomePageState extends State<MyHomePage> {
                           bool isSelected = device["deviceId"] == _selectedDeviceId;
                           return ListTile(
                             title: Text(device["name"] ?? "Unknown"),
-                            subtitle: Text(device["deviceId"] ?? ""),
                             selected: isSelected,
-                            selectedTileColor: Colors.lightBlueAccent,
+                            selectedTileColor: Colors.greenAccent,
                             onTap: () {
                               // Use setStateDialog to update the state within the dialog
                               setStateDialog(() {
@@ -117,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         }).toList(),
                       );
                     } else {
-                      return const Center(child: Text("No devices found."));
+                      return const Center(child: Text("No Sensors found."));
                     }
                   },
                 ),
@@ -158,6 +161,127 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget _buildSelectableChannel(int index) {
+    bool isSelected = _selectedChannelIndices.contains(index);
+    return InkWell(
+      onTap: () {
+        // Check if the condition to allow interaction is met
+        if (!_channelsConfirmed && (isSelected || _selectedChannelIndices.length < 4)) {
+          setState(() {
+            if (isSelected) {
+              _selectedChannelIndices.remove(index);
+            } else {
+              _selectedChannelIndices.add(index);
+            }
+          });
+        } else if(_channelsConfirmed) {
+          // Show a warning message if the condition is not met
+          Fluttertoast.showToast(
+            msg: "You have to reset to edit choices.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0,
+          );
+        } else {
+          // Show a warning message if the condition is not met
+          Fluttertoast.showToast(
+            msg: "You cannot choose more than 4 channels.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0,
+          );
+        }
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(
+            isSelected ? Icons.radio_button_checked : Icons.radio_button_unchecked,
+            color: isSelected ? Colors.greenAccent : Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              _channels[index],
+              style: const TextStyle(fontWeight: FontWeight.bold), // Make text bold
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildChannelSelectionGrid() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // Align content to the start
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 40.0, bottom: 12.0), // Add some space below the heading
+          child: Text(
+            'Choose Your Channels:',
+            style: Theme.of(context).textTheme.titleLarge, // Styling for the heading
+          ),
+        ),
+        Padding( // Apply padding to the Wrap widget to align it with the title
+          padding: const EdgeInsets.only(right: 25.0),
+          child: Wrap(
+            spacing: 20.0, // Horizontal space between buttons
+            runSpacing: 5.0, // Vertical space between lines
+            children: List<Widget>.generate(_channels.length, (index) {
+              return _buildSelectableChannel(index);
+            }),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildConfirmResetButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: !_channelsConfirmed && _selectedChannelIndices.isNotEmpty ? () {
+            setState(() {
+              _channelsConfirmed = true; // Confirm selection
+            });
+            // Optionally: Invoke logic to handle confirmed selection here
+          } : null, // Disable button if conditions are not met
+          style: ElevatedButton.styleFrom(
+            backgroundColor: !_channelsConfirmed && _selectedChannelIndices.isNotEmpty ? Colors.greenAccent : Colors.grey, // Color when disabled
+          ),
+          child: const Text(
+            'Confirm',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        const SizedBox(width: 20), // Space between buttons
+        ElevatedButton(
+          onPressed: _channelsConfirmed ? () {
+            setState(() {
+              _channelsConfirmed = false; // Reset confirmation
+              _selectedChannelIndices.clear(); // Clear selections
+            });
+          } : null, // Disable button if conditions are not met
+          style: ElevatedButton.styleFrom(
+            backgroundColor: _channelsConfirmed ? Colors.red : Colors.grey, // Color when disabled
+          ),
+          child: const Text(
+            'Reset',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     bool isConnected = _connectionStatus == "Connected";
@@ -168,18 +292,22 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             // Use a Row widget to place buttons next to each other
+            const Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 0.0)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center, // Center the row content
               children: <Widget>[
                 ElevatedButton(
                   onPressed: isConnected ? null : _startScan,
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: isConnected ? Colors.grey : Colors.blue, // Grey out if connected
+                    backgroundColor: isConnected ? Colors.grey : Colors.greenAccent, // Grey out if connected
                   ),
-                  child: const Text('Find a Device'),
+                  child: const Text('Find a Sensor',
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
                 const SizedBox(width: 20), // Provide some horizontal spacing between the buttons
                 ElevatedButton(
@@ -187,19 +315,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: isConnected ? Colors.red : Colors.grey, // Grey out if not connected
                   ),
-                  child: const Text('Unpair Device'),
+                  child: const Text('Unpair Sensor',
+                    style: TextStyle(color: Colors.black),
+                    ),
                 ),
               ],
             ),
             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text('Connection Status: $_connectionStatus',
-                style: TextStyle(
-                  fontSize: 15,
-                  color: isConnected ? Colors.green : _connectionStatus == "Connecting" ? Colors.orange : Colors.red,
+              padding: const EdgeInsets.all(8.0),
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(fontSize: 15, color: Colors.black), // Default text style
+                  children: <TextSpan>[
+                    const TextSpan(text: 'Connection Status: '), // Text in default style
+                    TextSpan(
+                      text: _connectionStatus, // Text that needs color
+                      style: TextStyle(
+                        color: isConnected ? Colors.green : _connectionStatus == "Connecting" ? Colors.orange : Colors.red, // Conditional color
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+            _buildChannelSelectionGrid(),
+            const Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 0.0)),
+            _buildConfirmResetButtons(),
           ],
         ),
       ),
